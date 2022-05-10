3e04aa9d3ebf3cc5aeb6df04a9cd430b67fedd19
Andrea Caggiani <caggianiandrea@gmail.com>
2022-05-10 09:55:03 +0200

    select supplier
    sketched function to select supplier, added file to elaborate information

--- index.php
+++ index.php
@@ -2,10 +2,20 @@
     require "php/function.php";
 
     echo "<title>SELEZIONA FORNITORE</title>";
-    $supplier=getSupplier();
-    echo "<select name='supplier'>";
-    foreach($supplier as $k=>$v){
-        echo "<option value='".$supplier[$k]->getID()."'>".$supplier[$k]->getNome()."</option>";
-    }
-    echo "</select>";
+
+    echo "<form action='php/work.php' method='post'>";
+        echo "<fieldset>";
+            echo "<legend>SELEZIONA FORNITORE</legend>";
+            echo "FORNITORE&nbsp;&nbsp;";
+            $supplier=getSupplier();
+            echo "<select name='supplier'>";
+            foreach($supplier as $k=>$v){
+                echo "<option value='".$supplier[$k]->getID()."'>".$supplier[$k]->getNome()."</option>";
+            }
+            echo "<option value='0000'>-- ALTRO --</option>";
+            echo "</select>";
+            echo "<br><br>NOME FORNITORE <input type='text'>";
+            echo "<br><br><input type='submit'>";
+        echo "</fieldset>";
+    echo "</form>";
 ?>
